20d91cd19ffe7b85bb0d4b494b31e0a5173398a4
Atharva Mishra <83823608+PyDev19@users.noreply.github.com>
2023-05-07 20:47:20 -0500

    added documentation to code
    added documentation to code to make it easier for contributors to make changes

--- mods.py
+++ mods.py
@@ -5,6 +5,18 @@ from bs4 import BeautifulSoup
 import time
 
 def download_mod(name: str, version: str, loader: str, loader_id: str, driver: webdriver.Edge, wait: bool = True) -> None:
+    """
+    Download a mod from CurseForge.
+
+    Args:
+        name (str): The name of the mod.
+        version (str): The version of the Minecraft game.
+        loader (str): The mod loader (forge, fabric, quilt).
+        loader_id (str): The ID corresponding to the mod loader.
+        driver (webdriver.Edge): The Edge WebDriver instance.
+        wait (bool, optional): Whether to wait for the download to complete (default is True).
+    """
+    
     # Construct the search URL for CurseForge using the mod name and version
     url = f'https://www.curseforge.com/minecraft/search?search={name}&page=1&class=mc-mods&gameFlavorsIds={loader_id}&sortType=1&pageSize=20'
 
@@ -28,7 +40,10 @@ def download_mod(name: str, version: str, loader: str, loader_id: str, driver: w
     # Find the first mod with the given name and navigate to its files page for the desired version
     mod = results_container.find('div', class_='project-card')
     mod_link = mod.find('a')['href']
-    driver.get(f'https://www.curseforge.com{mod_link}/files?version={version}')
+    if version == 0:
+        driver.get(f'https://www.curseforge.com{mod_link}/files')
+    else:
+        driver.get(f'https://www.curseforge.com{mod_link}/files?version={version}')
     driver.implicitly_wait(10)
 
     loaders_list = driver.find_element(By.CSS_SELECTOR, 'ul[class="dropdown-list"]')
@@ -43,13 +58,22 @@ def download_mod(name: str, version: str, loader: str, loader_id: str, driver: w
     mod_file = None
     found = False
 
-    for file_card in file_cards:
-        # Find the `game-version` element within the file card
-        game_version = file_card.find_element(By.CSS_SELECTOR, 'div[data-testid^="game-version"]')
-        game_version = game_version.find_element(By.TAG_NAME, 'span').text
+    if version != 0:
+        for file_card in file_cards:
+            # Find the `game-version` element within the file card
+            game_version = file_card.find_element(By.CSS_SELECTOR, 'div[data-testid^="game-version"]')
+            game_version = game_version.find_element(By.TAG_NAME, 'span').text
 
-        if version in game_version:
-            # If the file card matches the desired version, click on it and download the mod file
+            if version in game_version:
+                # If the file card matches the desired version, click on it and download the mod file
+                mod_file = file_card
+                mod_file.click()
+                found = True
+
+                mod_file = (driver.current_url).replace('files','download')
+                break
+    else:
+        for file_card in file_cards:
             mod_file = file_card
             mod_file.click()
             found = True
@@ -59,7 +83,10 @@ def download_mod(name: str, version: str, loader: str, loader_id: str, driver: w
 
     if not found:
         # If the desired version is not available, print an error message
-        print(f'{name.replace("+", " ")} is not available in version {version}')
+        if version == 0:
+            print(f'{name.replace("+", " ")} is not available for the latest Minecraft version')
+        else:
+            print(f'{name.replace("+", " ")} is not available for Minecraft version {version}')
     else:
         # Otherwise, navigate to the mod file download page and wait for the download to start
         driver.get(mod_file)
@@ -69,3 +96,4 @@ def download_mod(name: str, version: str, loader: str, loader_id: str, driver: w
             time.sleep(15)
         else:
             time.sleep(5)
+
