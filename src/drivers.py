dc843de95583c69df796a2bf21ccb681b490f426
PyDev19 <atharvamishra956+1@gmail.com>
2023-05-13 23:17:52 -0500

    created function to get driver and set driver options

--- src/drivers.py
+++ src/drivers.py
@@ -4,6 +4,20 @@ import io
 import subprocess
 
 def get_browser_version(browser_name):
+    """
+    Get the version of the specified browser.
+
+    Args:
+        browser_name (str): Name of the browser (e.g., 'chrome', 'firefox').
+
+    Returns:
+        tuple: A tuple containing the browser name and version (browser_name, version),
+               or None if the browser is not found.
+
+    Raises:
+        subprocess.CalledProcessError: If the subprocess call fails.
+        FileNotFoundError: If the browser executable is not found.
+    """
     browser_name = browser_name.lower()
     browser_name = browser_name + "-browser"
 
@@ -20,33 +34,100 @@ def get_browser_version(browser_name):
     except (subprocess.CalledProcessError, FileNotFoundError):
         return None
     
-def get_browser_driver(browser_name):
-    browser_name, browser_version = get_browser_version(browser_name)
-    
-    if browser_name == 'chromium' or browser_name == 'chrome':
-        driver_url = f'https://chromedriver.storage.googleapis.com/{browser_version}/chromedriver_linux64.zip'
+def download_browser_driver(browser_name):
+    """
+    Download the appropriate browser driver for the specified browser and extract it.
 
-    # Directory to extract the contents
+    Args:
+        browser_name (str): Name of the browser (e.g., 'chrome', 'firefox').
+
+    Returns:
+        bool: True if the download and extraction were successful, False otherwise.
+    """
+    browser_name, browser_version = get_browser_version(browser_name)
+
+    if browser_name not in ['chromium', 'chrome']:
+        print(f'Unsupported browser: {browser_name}')
+        return False
+
+    driver_url = f'https://chromedriver.storage.googleapis.com/{browser_version}/chromedriver_linux64.zip'
     extract_directory = 'drivers/'
 
-    # Download the zip file
-    response = requests.get(driver_url)
-    
-    print(f'Downloading {browser_name} driver')
+    try:
+        response = requests.get(driver_url)
+        response.raise_for_status()  # Raise an exception for non-200 status codes
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Create a ZipFile object from the downloaded content
-        zip_file = zipfile.ZipFile(io.BytesIO(response.content))
-        
-        # Extract the contents of the zip file to the specified directory
-        zip_file.extractall(extract_directory)
-        
-        # Close the ZipFile object
-        zip_file.close()
-        
+        print(f'Downloading {browser_name} driver')
+
+        with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
+            zip_file.extractall(extract_directory)
+
         print(f'Done downloading {browser_name} driver')
         return True
-    else:
-        print(f'Failed to download {browser_name} driver')
+    except (requests.RequestException, zipfile.BadZipFile) as e:
+        print(f'Error occurred while downloading {browser_name} driver: {str(e)}')
         return False
+
+def set_driver_options(browser):
+    """
+    Set up the driver options for the specified browser.
+
+    Args:
+        browser (str): Selection of the browser (1 for Chrome, 2 for Chromium).
+
+    Returns:
+        tuple: A tuple containing the WebDriver instance and the download directory.
+               If the browser selection is unsupported, returns (None, None).
+    """
+    download_dir = input('Enter the download directory you want to download to (leave empty for default): ')
+    headless = input('Do you want to see the browser window? (y/n): ')
+
+    options = None
+    service = None
+
+    if browser in ['1', 'chrome']:
+        from selenium.webdriver.chrome.options import Options
+        from selenium.webdriver.chrome.service import Service
+
+        if download_browser_driver('chrome'):
+            # Set up the Chrome driver with options to enable JavaScript and cookies
+            options = Options()
+            options.add_argument('--enable-javascript')
+            options.add_argument('--enable-cookies')
+            service = Service(executable_path='drivers/chromedriver')
+
+    elif browser in ['2', 'chromium']:
+        from selenium.webdriver.chromium.options import ChromiumOptions
+        from selenium.webdriver.chromium.service import ChromiumService
+
+        if download_browser_driver('chromium'):
+            # Set up the Chromium driver with options to enable JavaScript and cookies
+            options = ChromiumOptions()
+            options.add_argument('--enable-javascript')
+            options.add_argument('--enable-cookies')
+            service = ChromiumService(executable_path='drivers/chromedriver')
+
+    if options is None or service is None:
+        print('Unsupported browser selection')
+        return None, None
+
+    if download_dir:
+        # Set the download directory preference if provided
+        prefs = {
+            'download.default_directory': download_dir,
+            'download.prompt_for_download': False,
+            'safebrowsing.enabled': False,  # Disable the safe browsing warning
+            'profile.default_content_settings.popups': 0
+        }
+        options.add_experimental_option('prefs', prefs)
+        options.add_argument('--safebrowsing-disable-download-protection')
+
+    if headless == 'n':
+        # Set the user agent to mimic a specific web browser (Chrome in this case)
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+        options.add_argument(f'user-agent={user_agent}')
+        options.add_argument('--headless')
+
+    
+
+    return service, options, download_dir
